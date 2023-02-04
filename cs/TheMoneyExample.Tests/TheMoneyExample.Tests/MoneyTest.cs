1761e51496f95f37cfda499508af8157378b6ed8
pixcelo <bodomu@gmail.com>
2023-02-05 05:33:20 +0900

    Dollar

--- cs/TheMoneyExample.Tests/TheMoneyExample.Tests/MoneyTest.cs
+++ cs/TheMoneyExample.Tests/TheMoneyExample.Tests/MoneyTest.cs
@@ -5,7 +5,7 @@ namespace TheMoneyExample.Tests
         [Fact]
         public void TestMultiplication()
         {
-            Dollar five = new Dolloar(5);
+            Dollar five = new Dollar(5);
             five.Times(2);
             Assert.Equal(10, five.amount);
         }
