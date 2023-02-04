4498125d8b167b25f84585623a735d3f09689d3d
pixcelo <bodomu@gmail.com>
2023-02-05 05:54:33 +0900

    コンパイルエラーが消えてテストが失敗したことを確認する

--- cs/TheMoneyExample/TheMoneyExample/Dollar.cs
+++ cs/TheMoneyExample/TheMoneyExample/Dollar.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheMoneyExample
+﻿namespace TheMoneyExample
 {
     public class Dollar
     {
+        public int amount;
+
+        public Dollar(int amount)
+        {
+
+        }
+
+        public void Times(int multiplier)
+        {
+
+        }
     }
 }
