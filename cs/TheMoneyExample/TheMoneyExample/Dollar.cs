ec0beebeab74cc755ccae6b4efcafb94d8faf57b
pixcelo <bodomu@gmail.com>
2023-02-07 04:59:48 +0900

    オブジェクトの等価性テスト

--- cs/TheMoneyExample/TheMoneyExample/Dollar.cs
+++ cs/TheMoneyExample/TheMoneyExample/Dollar.cs
@@ -17,5 +17,11 @@
         {
             return new Dollar(amount * multiplier);
         }
+
+        public override bool Equals(object obj)
+        {
+            Dollar dollar = (Dollar)obj;
+            return amount == dollar.amount;
+        }
     }
 }
