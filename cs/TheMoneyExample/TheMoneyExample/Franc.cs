e1362c506f25fe1f0d94be0838b11aece8f32230
pixcelo <bodomu@gmail.com>
2023-02-08 05:33:58 +0900

    サブクラス化

--- cs/TheMoneyExample/TheMoneyExample/Franc.cs
+++ cs/TheMoneyExample/TheMoneyExample/Franc.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheMoneyExample
+﻿namespace TheMoneyExample
 {
-    public class Franc
-    {
-        private readonly int amount;
-        
+    public class Franc : Money
+    {        
         public Franc(int amount)
         {
             this.amount = amount;
@@ -18,12 +10,6 @@ namespace TheMoneyExample
         public Franc Times(int multiplier)
         {
             return new Franc(amount * multiplier);
-        }
-
-        public override bool Equals(object obj)
-        {
-            Franc franc = (Franc)obj;
-            return amount == franc.amount;
         }
     }
 }
