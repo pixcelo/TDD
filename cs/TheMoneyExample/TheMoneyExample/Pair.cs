5602e7221fcf809cd84773f242954f80ea7d837f
pixcelo <bodomu@gmail.com>
2023-02-14 06:41:47 +0900

    ハッシュテーブルのPairクラス

---
+++ cs/TheMoneyExample/TheMoneyExample/Pair.cs
@@ -0,0 +1,25 @@
+﻿namespace TheMoneyExample
+{
+    public class Pair
+    {
+        private string from;
+        private string to;
+
+        public Pair(string from, string to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+           
+        public override bool Equals(Object obj)
+        {
+            Pair pair = (Pair)obj;
+            return from.Equals(pair.from) && to.Equals(pair.to);
+        }
+
+        public int HashCode()
+        {
+            return 0;
+        }
+    }
+}
