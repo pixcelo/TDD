471c55631011ac56af52cf1cd3d7744558eeae83
pixcelo <bodomu@gmail.com>
2023-02-23 15:39:51 +0900

    Sum Timesテスト

--- cs/TheMoneyExample.Tests/TheMoneyExample.Tests/MoneyTest.cs
+++ cs/TheMoneyExample.Tests/TheMoneyExample.Tests/MoneyTest.cs
@@ -100,5 +100,17 @@ namespace TheMoneyExample.Tests
             Money result = bank.Reduce(sum, "USD");
             Assert.Equal(Money.Dollar(15), result);
         }
+
+        [Fact]
+        public void TestSumTimes()
+        {
+            Expression fiveBucks = Money.Dollar(5);
+            Expression tenFrancs = Money.Franc(10);
+            Bank bank = new Bank();
+            bank.AddRate("CHF", "USD", 2);
+            Expression sum = new Sum(fiveBucks, tenFrancs).Times(2);
+            Money result = bank.Reduce(sum, "USD");
+            Assert.Equal(Money.Dollar(20), result);
+        }
     }
 }
