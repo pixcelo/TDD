d62af3865fc61e9744f1193c769c227edab472a1
pixcelo <bodomu@gmail.com>
2023-02-26 04:16:06 +0900

    setupメソッドの中でインスタンスを作成して使い回す

--- py/xunit.py
+++ py/xunit.py
@@ -11,25 +11,22 @@ class TestCase:
 
 # 子クラス
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.WasRun = None
-        super().__init__(name)
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
     def testMethod(self):
         self.WasRun = 1
 
 # TestCaseクラスのテスト
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.WasRun)
-        test.run()
-        assert(test.WasRun)
+        self.test.run()
+        assert(self.test.WasRun)
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
